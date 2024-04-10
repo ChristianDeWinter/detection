9acde5dfefd3db12d9d16786b8ca24460a20f095
ChristianDeWinter <113032930+ChristianDeWinter@users.noreply.github.com>
2024-04-10 13:43:09 +0200

    Update test object detection.py

--- test object detection.py
+++ test object detection.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import torch
 
 # Initialize YOLO model
-model = YOLO('model/fortnitev8.pt')
+model = YOLO('yolov8n.pt')
 
 # Move the model to CUDA if available
 if torch.cuda.is_available():
@@ -12,7 +12,7 @@ else:
     print("Cuda NOT ACTIVE")
 
 # Perform object detection
-result = model(source='video/fortnite.mov', task='detect', show=True, conf=0.2, save=False)
+result = model(source='newyork.png', task='detect', show=True, conf=0.2, save=False)
 
 # Wait for user input before exiting
 input("Press Enter to exit...")
