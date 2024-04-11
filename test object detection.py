de479bdb3bf218f9cad4793b8133879d979f6027
ChristianDeWinter <113032930+ChristianDeWinter@users.noreply.github.com>
2024-04-11 10:13:01 +0200

    new commit

--- test object detection.py
+++ test object detection.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import torch
 
 # Initialize YOLO model
-model = YOLO('yolov8n.pt')
+model = YOLO('model/yolov8n-pose.pt')
 
 # Move the model to CUDA if available
 if torch.cuda.is_available():
@@ -12,7 +12,7 @@ else:
     print("Cuda NOT ACTIVE")
 
 # Perform object detection
-result = model(source='newyork.png', task='detect', show=True, conf=0.2, save=False)
+result = model(source='video/pushup.mp4', task='detect', show=True, conf=0.2, save=False)
 
 # Wait for user input before exiting
 input("Press Enter to exit...")
