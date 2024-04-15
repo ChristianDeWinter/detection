270baa3f5de08e58db544e47f9065ee978c580a6
ChristianDeWinter <113032930+ChristianDeWinter@users.noreply.github.com>
2024-04-15 13:28:59 +0200

    new

--- test object detection.py
+++ test object detection.py
@@ -1,18 +1,36 @@
-from ultralytics import YOLO
+import os
+import cv2
 import torch
+import numpy as np
+import math
+import json
+import datetime
+import argparse
+from ultralytics import YOLO
 
-# Initialize YOLO model
-model = YOLO('model/yolov8n-pose.pt')
-
-# Move the model to CUDA if available
-if torch.cuda.is_available():
-    print("Cuda Active")
-    model = model.to('cuda')
-else:
-    print("Cuda NOT ACTIVE")
-
-# Perform object detection
-result = model(source='video/pushup.mp4', task='detect', show=True, conf=0.2, save=False)
-
-# Wait for user input before exiting
-input("Press Enter to exit...")
+sport_list = {
+    'situp': {
+        'left_points_idx': [6, 12, 14],
+        'right_points_idx': [5, 11, 13],
+        'maintaining': 70,
+        'relaxing': 110,
+        'concerned_key_points_idx': [5, 6, 11, 12, 13, 14],
+        'concerned_skeletons_idx': [[14, 12], [15, 13], [6, 12], [7, 13]]
+    },
+    'pushup': {
+        'left_points_idx': [6, 8, 10],
+        'right_points_idx': [5, 7, 9],
+        'maintaining': 140,
+        'relaxing': 120,
+        'concerned_key_points_idx': [5, 6, 7, 8, 9, 10],
+        'concerned_skeletons_idx': [[9, 11], [7, 9], [6, 8], [8, 10]]
+    },
+    'squat': {
+        'left_points_idx': [11, 13, 15],
+        'right_points_idx': [12, 14, 16],
+        'maintaining': 80,
+        'relaxing': 140,
+        'concerned_key_points_idx': [11, 12, 13, 14, 15],
+        'concerned_skeletons_idx': [[16, 14], [14, 12], [17, 15], [15, 13]]
+    }
+}
